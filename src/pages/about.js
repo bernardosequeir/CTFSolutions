1501aabaad030e466d8649c515ef0a318d8ae052
bernardosequeir <bernardosequeir@gmail.com>
2020-02-28 13:49:42 +0000

    added info to the about page, still need to add links

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,10 +5,13 @@ import Layout from '../components/layout';
 
 export default ({ data }) => (
   <Layout>
-    <h1>About {data.site.siteMetadata.title}</h1>
-    <p>
-      We're the only site running on your computer dedicated to showing the best
-      photos and videos of pandas eating lots of food.
+    <h1 className="headline1">About {data.site.siteMetadata.title}</h1>
+    <p className="body-1">
+      Hi! This is my blog. I'm currently finishing up my degree in Computer
+      Science. I'm still a noob in infosec and english is not my first language
+      so please excuse any basic mistake in grammar. I will mostly be writing
+      about security CTFs and books here, catch me on twitter at @bernardoseqsec
+      for all the other stuff.
     </p>
   </Layout>
 );
